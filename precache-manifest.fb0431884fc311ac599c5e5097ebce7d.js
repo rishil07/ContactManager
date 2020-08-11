7590ee0ac35bc862f9db7dc12d13d2f40697add5
xoriant120 <xoriant120@iiht.tech>
2020-08-11 09:54:31 +0530

    Updates

---
+++ precache-manifest.fb0431884fc311ac599c5e5097ebce7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5c60816f3dd89c652a8402e022d9d16",
+    "url": "/ContactManager/index.html"
+  },
+  {
+    "revision": "da62d831878e50fd50f8",
+    "url": "/ContactManager/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "da62d831878e50fd50f8",
+    "url": "/ContactManager/static/js/2.7597a338.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ContactManager/static/js/2.7597a338.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1e9a2d6817ce5247f25",
+    "url": "/ContactManager/static/js/main.a17b9276.chunk.js"
+  },
+  {
+    "revision": "6453ca79e2239f4879f8",
+    "url": "/ContactManager/static/js/runtime-main.d7939588.js"
+  }
+]);
